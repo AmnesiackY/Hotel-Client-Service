76104beee6f47fb78682e416aa8d20de9d43cd21
Amne <amne@test.com>
2025-12-27 02:44:35 +0100

    add flyway + 2 migrations

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.flywaydb:flyway-core")
 
     runtimeOnly("org.postgresql:postgresql")
 }
---
+++ src/main/resources/db/migration/V2__add_phone_to_clients.sql
@@ -0,0 +1,2 @@
+ALTER TABLE clients
+ADD COLUMN phone VARCHAR(32);
