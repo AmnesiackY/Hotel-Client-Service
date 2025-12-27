4c688190c6843efb99414d5df620767256d785e9
Amne <amne@test.com>
2025-12-27 02:31:45 +0100

    migrate from H2 to Postgres (In memory DB -> Postgres + Docker)

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,8 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    runtimeOnly("com.h2database:h2")
+
+    runtimeOnly("org.postgresql:postgresql")
 }
 
 java {
