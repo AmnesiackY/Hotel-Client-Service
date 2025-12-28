a9fda9a68e754ef6dae662459f2f7bb68327a65a
Amne <hitsac2711@gmail.com>
2025-12-28 22:34:01 +0100

    delete legacy files

--- src/main/kotlin/com/yarek/hotel/repository/ClientRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.yarek.hotel.repository
-
-import com.yarek.hotel.entity.ClientEntity
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface ClientRepository : JpaRepository<ClientEntity, Long>
