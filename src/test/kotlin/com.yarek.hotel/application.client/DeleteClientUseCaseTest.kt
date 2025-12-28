3d779ac179ad5ae4afc5feb60ba02dda872ffd87
Amne <hitsac2711@gmail.com>
2025-12-28 22:21:04 +0100

    Add unit tests for DeleteUseCase

--- src/test/kotlin/com.yarek.hotel/application.client/DeleteClientUseCaseTest.kt
+++ src/test/kotlin/com.yarek.hotel/application.client/DeleteClientUseCaseTest.kt
@@ -1,16 +1,39 @@
 package com.yarek.hotel.application.client
 
+import com.yarek.hotel.domain.client.Client
+import com.yarek.hotel.domain.client.ClientRepository
+import com.yarek.hotel.exception.ClientNotFoundException
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class DeleteClientUseCaseTest {
 
     @Test
-    fun `should delete client by id`() {
-        //TODO Test implementation goes here
+    fun `should delete client when exists`() {
+        val repository = object : ClientRepository {
+            override fun deleteById(id: Long): Boolean = true
+            override fun save(client: Client): Client = client
+            override fun findById(id: Long): Client? = null
+        }
+
+        val useCase = DeleteClientUseCase(repository)
+
+        // should not throw
+        useCase.execute(1L)
     }
 
     @Test
-    fun `should handle deletion of non-existing client`() {
-        //TODO Test implementation goes here
+    fun `should throw exception when client does not exist`() {
+        val repository = object : ClientRepository {
+            override fun deleteById(id: Long): Boolean = false
+            override fun save(client: Client): Client = client
+            override fun findById(id: Long): Client? = null
+        }
+
+        val useCase = DeleteClientUseCase(repository)
+
+        assertThrows<ClientNotFoundException> {
+            useCase.execute(999L)
+        }
     }
 }
